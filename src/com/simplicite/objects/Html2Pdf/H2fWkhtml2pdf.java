adbc1972ff1d193930a115dfbe57b0a21e5e3a41
designer (Designer) <no-reply@simplicite.fr>
2021-03-31 01:41:29 +0200

    add HTMLToPDFTool

--- src/com/simplicite/objects/Html2Pdf/H2fWkhtml2pdf.java
+++ src/com/simplicite/objects/Html2Pdf/H2fWkhtml2pdf.java
@@ -12,9 +12,20 @@ public class H2fWkhtml2pdf extends ObjectDB {
 	private static final long serialVersionUID = 1L;
 	
 	public String makePdf() {
-		getField("h2pWkFile").setDocument(this, "test.pdf", pubPdf(getFieldValue("h2pWkUrl"), getFieldValue("h2pWkHtml")));
+		getField("h2pWkFile").setDocument(this, "wkhtml2pdf.pdf", pubPdf(getFieldValue("h2pWkUrl"), getFieldValue("h2pWkHtml")));
+		getField("h2pWkTool").setDocument(this, "HTMLToPDFTool.pdf", pubPdfTool(getFieldValue("h2pWkHtml")));
 		save();
 		return null;
+	}
+	
+	public byte[] pubPdfTool(String html){
+		try{
+			return HTMLToPDFTool.toPDF(html);
+		}
+		catch(Exception e){
+			AppLog.error(getClass(), "pubPdf", "------------", e, getGrant());
+			return null;
+		}
 	}
 	
 	public byte[] pubPdf(String url, String html){
